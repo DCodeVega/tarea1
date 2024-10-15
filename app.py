45e61fa9686bdf0db855315cebd5aef091dedb62
Diego Vega <escorpiotauro2015@gmail.com>
2024-10-15 11:46:37 -0400

    segundo commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        
+        return render_template('contact.html', message="Gracias por tu mensaje!")
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
